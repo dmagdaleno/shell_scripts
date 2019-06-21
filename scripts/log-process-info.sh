a9805435fc5dfd7ed3e7e2ed36d743c0ab652f79
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-06-21 18:42:09 -0300

    log process info to a file

--- scripts/log-process-info.sh
+++ scripts/log-process-info.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ ! -d logs ]
+then
+        mkdir logs
+fi
+
 pids=$(ps -e -o pid --sort -size | grep [0-9] | head)
 
 for pid in $pids
 do
-	printf '%s\n' $(ps -p $pid -o comm=)
+	p_name=$(ps -p $pid -o comm=)
+	date_time=$(date +%F,%H:%M:%S)
+	echo $date_time >> logs/$p_name.log
 done
