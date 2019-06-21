822ae0a6e4ee8afd34156a48acba460ec2125028
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-06-21 19:03:33 -0300

    add size of process

--- scripts/log-process-info.sh
+++ scripts/log-process-info.sh
@@ -10,6 +10,8 @@ pids=$(ps -e -o pid --sort -size | grep [0-9] | head)
 for pid in $pids
 do
 	p_name=$(ps -p $pid -o comm=)
+	p_size=$(ps -p $pid -o size=)
+	p_size_mb=$(bc <<< "scale=2; $p_size/1024")
 	date_time=$(date +%F,%H:%M:%S)
-	echo $date_time >> logs/$p_name.log
+	echo $date_time,$p_size_mb MB >> logs/$p_name.log
 done
