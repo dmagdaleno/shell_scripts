02c17b3d3c5ce33111d89cf6223ee25100e0f796
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-06-21 08:56:11 -0300

    extract script to a function

--- scripts/convert-images.sh
+++ scripts/convert-images.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-cd ~/work/shell_scripts/images
+convert_images(){
+	cd ~/work/shell_scripts/images
 
-if [ ! -d $2 ]
-then
-	mkdir $2
-fi
+	if [ ! -d $2 ]
+	then
+		mkdir $2
+	fi
 
-for file in *.$1
-do
-	file_name=$(ls $file | awk -F. '{ print $1 }')
-	convert $file $2/$file_name.$2
-done
+	for file in *.$1
+	do
+		local file_name=$(ls $file | awk -F. '{ print $1 }')
+		convert $file $2/$file_name.$2
+	done
+}
+
+convert_images $@
